090ac85909da353d07b60a3d6a08b941929a3390
Pablo Galinanes <9001195@alumnos.ufv.es>
2022-10-26 18:34:11 +0200

    v1.0 release
    Added a menu interface to  the application. Added multiple options as scanning the default user downloads folder or selecting a directory manually using a zenity popup

---
+++ Wuolah Remover v1.0.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+# Needs gualcleaner, pip, zenity installed
+clear
+echo -e "================================================.\n     .-.   .-.     .--.                         |\n    | OO| | OO|   / _.-' .-.   .-.  .-.   .''.  |\n    |   | |   |   \  '-. '-'   '-'  '-'   '..'  |\n    '^^^' '^^^'    '--'                         |\n===============.  .-.  .================.  .-.  |\n               | |   | |                |  '-'  |\n               | |   | |                |       |\n               | ':-:' |                |  .-.  |\n               |  '-'  |                |  '-'  |\n==============='       '================'       |\n"
+echo -e "Porfavor escoja una opción:"
+#PS3='Porfavor escoja una opción' \n
+options=("Escanear carpeta Descargas" "Arrastrar archivos" "Seleccionar direcotrio" "Salir")
+select opt in "${options[@]}"
+do
+    case $opt in
+        "Escanear carpeta Descargas")
+            find /Users/username/Downloads/ -name "wuolah-free-*.pdf" -print0 | while read -d $'\0' file
+                do
+                    gulagcleaner "$file"
+                done
+
+                exit
+            ;;
+        "Arrastrar archivos")
+            echo "Hola! Arrastra y suelta los archivos que quieras aqui:"
+            IFS="" read -r input
+            eval "files=( $input )"
+
+            for file in "${files[@]}"
+            do
+                 gulagcleaner "$file"
+                 #rm "$file"
+            done
+
+            exit
+            ;;
+        "Seleccionar direcotrio")
+                
+                DIR=$(zenity --file-selection \
+                    --title="Wuolah Remover" \
+                    --height=200 \
+                    --width=100 \
+                    --text="Selecciona un directorio:" \
+                    --directory)
+            find $DIR -name "wuolah-free-*.pdf" -print0 | while read -d $'\0' file
+                do
+                    gulagcleaner "$file"
+                done
+
+                exit
+            ;;
+        "Salir")
+            break
+            ;;
+        *) echo "Opción inválida $REPLY";;
+    esac
+done
